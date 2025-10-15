9835809911c22db6e88d7f7e78333d91463873fd
TakerVare <takervare@gmail.com>
2025-10-15 19:23:43 +0200

    Empiezo con el controlador de categorías

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Mvc;
+using Reto_0_Backend.Models;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+
+
+namespace Reto_0_Backend.Controllers;
+
+[ApiController]
+[Route("[Category]")]
+public class CategoryController : ControllerBase
+{
+    /*
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+    */
+
+    private DataCollectionExample dataCollectionExample = new DataCollectionExample();
+
+    private readonly ILogger<CategoryController> _logger;
+
+    public CategoryController(ILogger<CategoryController> logger)
+    {
+        _logger = logger;
+    }
+
+    
+    [HttpGet]
+    public  IEnumerable<Category> Get()
+    {
+                
+        return dataCollectionExample.CategoryCollectionList.ToArray();
+        
+        /*
+        return Enumerable.Range(1, 5).Select(index => new Category
+        {
+            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        })
+        .ToArray();
+        */
+    }
+}
